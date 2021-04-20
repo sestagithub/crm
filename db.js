de67239f9d206ba4ef0436b7b0a4fbf2639aee72
SeSTA <info@sesta.org>
2021-04-20 13:43:12 +0530

    'g'

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 var mysql = require('mysql');
 
-//  const db = mysql.createConnection({
-//    host     : 'z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//    user     : 'he97ctpf5xhd2dx8',
-//    password : 'bhzu9o231663mvei',
-//    database : 'qio7bkfzsnpwdsgd', 
-//    port:3306,
+ const db = mysql.createConnection({
+   host     : 'z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+   user     : 'he97ctpf5xhd2dx8',
+   password : 'bhzu9o231663mvei',
+   database : 'qio7bkfzsnpwdsgd', 
+   port:3306,
   
-//  });
+ });
+
+
 
  const db = mysql.createConnection({
   host     : 'localhost',
